67b1ed2175e064b23b4b707a86ccd7f05ed6794c
Andy <mail@andrewgriffithsonline.com>
2014-02-02 22:25:21 +0000

    first commit

---
+++ tmpl/graph.tmpl.js
@@ -0,0 +1,20 @@
+module.exports = function anonymous(obj) {
+
+  function escape(html) {
+    return String(html)
+      .replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  };
+
+  function section(obj, prop, negate, str) {
+    var val = obj[prop];
+    if ('function' == typeof val) return val.call(obj, str);
+    if (negate) val = !val;
+    if (val) return str;
+    return '';
+  };
+
+  return "<div id=\"d3-graph-head\"></div>\n<div id=\"d3-graph\"></div>\n<div id=\"d3-metadata\"></div>	"
+}
